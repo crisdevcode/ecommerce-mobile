5428ea1c2917c7169959357f8dab8e0306bd792f
Cristian Unigarro <ungcrisfit@gmail.com>
2023-03-07 16:37:06 -0500

    add: [main food pag] - header

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,4 +1,7 @@
+import 'package:delivery_ecommerce/widgets/big_text.dart';
+import 'package:delivery_ecommerce/widgets/small_text.dart';
 import 'package:flutter/material.dart';
+import 'package:delivery_ecommerce/utils/colors.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({Key? key}) : super(key: key);
@@ -11,25 +14,35 @@ class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Row(
-          children: [
-            Column(
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 45, bottom: 15),
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('Country'),
-                Text('City'),
+                Column(
+                  children: [
+                    BigText(text: 'Colombia', color: AppColors.mainColor),
+                    SmallText(text: 'Ipiales', color: Colors.black54),
+                  ],
+                ),
+                Center(
+                  child: Container(
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppColors.mainColor
+                    ),
+                    child: const Icon(Icons.search, color: Colors.white),
+                  ),
+                )
               ],
             ),
-            Container(
-              width: 45,
-              height: 45,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.blue
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       )
     );
   }
