4ebdc23a12b6f1c92338b9ece9c31b7fd1879ec3
Cristian Unigarro <ungcrisfit@gmail.com>
2023-03-11 09:40:42 -0500

    add: popular food detail - counter functionality

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:delivery_ecommerce/data/repository/popular_product_repo.dart';
 import 'package:delivery_ecommerce/models/product_model.dart';
-import 'package:get/get.dart';
+import 'package:delivery_ecommerce/utils/colors.dart';
 
 class PopularProductController extends GetxController {
   final PopularProductRepo popularProductRepo;
@@ -13,6 +15,9 @@ class PopularProductController extends GetxController {
   bool _isLoaded = false;
   bool get isLoaded => _isLoaded;
 
+  int _quantity = 0;
+  int get quantity => _quantity;
+
   Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
     if (response.statusCode == 200) {
@@ -22,5 +27,35 @@ class PopularProductController extends GetxController {
       _isLoaded = true;
       update();
     } else {}
+  }
+
+  void setQuantity(bool isIncrement) {
+    if (isIncrement) {
+      _quantity = checkQuantity(_quantity + 1);
+    } else {
+      _quantity = checkQuantity(_quantity - 1);
+    }
+
+    update(); // Refresh state
+  }
+
+  int checkQuantity(int quantity) {
+    if (quantity < 0) {
+      if (quantity < 0) {
+        Get.snackbar("Cantidad de productos", "¡No puedes reducir más!",
+            backgroundColor: AppColors.mainColor, colorText: Colors.white);
+      }
+      return 0;
+    } else if (quantity > 20) {
+      Get.snackbar("Cantidad de productos", "¡No puedes agregar más!",
+          backgroundColor: AppColors.mainColor, colorText: Colors.white);
+      return 20;
+    } else {
+      return quantity;
+    }
+  }
+
+  void initProduct() {
+    _quantity = 0;
   }
 }
