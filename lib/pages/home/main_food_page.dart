eef11964ec5c9eb16d5911387b588c53c0eb90e5
Cristian Unigarro <ungcrisfit@gmail.com>
2023-03-17 21:06:15 -0500

    add: RefreshIndicator

--- lib/pages/home/main_food_page.dart
+++ lib/pages/home/main_food_page.dart
@@ -1,9 +1,12 @@
+import 'package:delivery_ecommerce/controllers/popular_product_controller.dart';
+import 'package:delivery_ecommerce/controllers/recommended_product_controller.dart';
 import 'package:delivery_ecommerce/pages/home/food_page_body.dart';
 import 'package:delivery_ecommerce/utils/dimensions.dart';
 import 'package:delivery_ecommerce/widgets/big_text.dart';
 import 'package:delivery_ecommerce/widgets/small_text.dart';
 import 'package:flutter/material.dart';
 import 'package:delivery_ecommerce/utils/colors.dart';
+import 'package:get/get.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({Key? key}) : super(key: key);
@@ -13,50 +16,59 @@ class MainFoodPage extends StatefulWidget {
 }
 
 class _MainFoodPageState extends State<MainFoodPage> {
+
+  Future<void> _loadResource() async {
+    await Get.find<PopularProductController>().getPopularProductList();
+    await Get.find<RecommendedProductController>().getRecommendedProductList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          // Showing the header
-          Container(
-            margin: EdgeInsets.only(top: Dimensions.height45, bottom: Dimensions.height15),
-            padding: EdgeInsets.only(left: Dimensions.width20, right: Dimensions.width20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    BigText(text: 'Colombia', color: AppColors.mainColor),
-                    Row(
-                      children: [
-                        SmallText(text: 'Ipiales', color: Colors.black54),
-                        const Icon(Icons.arrow_drop_down_rounded)
-                      ],
-                    ),
-                  ],
-                ),
-                Center(
-                  child: Container(
-                    width: Dimensions.height45,
-                    height: Dimensions.height45,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(Dimensions.radius15),
-                      color: AppColors.mainColor
-                    ),
-                    child: Icon(Icons.search, color: Colors.white, size: Dimensions.iconSize24),
+      body: RefreshIndicator(
+        onRefresh: _loadResource,
+        child: Column(
+          children: [
+            // Showing the header
+            Container(
+              margin: EdgeInsets.only(top: Dimensions.height45, bottom: Dimensions.height15),
+              padding: EdgeInsets.only(left: Dimensions.width20, right: Dimensions.width20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: 'Colombia', color: AppColors.mainColor),
+                      Row(
+                        children: [
+                          SmallText(text: 'Ipiales', color: Colors.black54),
+                          const Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      ),
+                    ],
                   ),
-                )
-              ],
+                  Center(
+                    child: Container(
+                      width: Dimensions.height45,
+                      height: Dimensions.height45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(Dimensions.radius15),
+                        color: AppColors.mainColor
+                      ),
+                      child: Icon(Icons.search, color: Colors.white, size: Dimensions.iconSize24),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          // Showing the body
-          const Expanded(
-            child: SingleChildScrollView(
-              child: FoodPageBody()
-            ),
-          )
-        ],
+            // Showing the body
+            const Expanded(
+              child: SingleChildScrollView(
+                child: FoodPageBody()
+              ),
+            )
+          ],
+        ),
       )
     );
   }
